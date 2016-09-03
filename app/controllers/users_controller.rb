139ea07c6ff87932b306713c605289bd77abd7f9
Morley Tatro <rippinradio@yahoo.com>
2016-09-03 13:41:20 -0700

    wired up Firebase login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
-  def index
-  end
+	def login
+	end
 
-  def show
-  end
+	def create
+		user = User.where("firebase_id = ? or email = ?", params[:user][:firebase_id], params[:user][:email])[0]
+		if user
+			user.update(firebase_id:params[:user][:firebase_id], handle:params[:user][:handle], email:params[:user][:email], avatar:params[:user][:avatar])
+		else
+			user = User.create(user_params)
+		end
+		session[:user_id] = user.id
+		render json: { success: true }
+	end
+
+	def index
+	end
+
+	def show
+	end
 end
